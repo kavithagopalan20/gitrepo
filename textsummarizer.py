2ba15b5dec98a95c29834802ec4c8291683c021f
kavithagopalan20 <kavithagopalan20@gmail.com>
2025-11-02 13:59:11 -0500

    Update textsummarizer.py
    updated summary input text

--- textsummarizer.py
+++ textsummarizer.py
@@ -5,7 +5,7 @@ client = OpenAI()
 try:
     response = client.responses.create(
         model="gpt-4.1-mini",
-        input="Summarize this text: AI is transforming industries..."
+        input="Summarize this text: AI is transforming major industriws and it this si cool...."
     )
     print(response.output[0].content[0].text)
 except APIError as e:

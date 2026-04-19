def92058f5985f47f86c71f202cb9556e72ab18d
kavithagopalan20 <kavithagopalan20@gmail.com>
2026-04-19 19:59:23 -0400

    Add files via upload
    " Code to reverse  a string"

---
+++ reversestring.py
@@ -0,0 +1,7 @@
+def reverse_string_simple(text):
+    # [start:stop:step] -1 means start from the end and step backward
+    return text[::-1]
+
+# Quick Check
+print(reverse_string_simple("Reverse this string")) 
+# Output: gnirts siht esreveR
